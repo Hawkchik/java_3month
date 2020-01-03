83e113cca8102f1d40432f9a4eecc50fa3dc86a8
hawkchiki <vakionika@gmail.com>
2020-01-03 15:59:03 +0300

    Some fix

--- src/Operations.java
+++ src/Operations.java
@@ -1,50 +1,84 @@
 import java.math.BigDecimal;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Queue;
+import java.util.*;
 
 public class Operations {
 
-    Deque<Transactions> queuesell = new ArrayDeque<>();
-    Deque<Transactions> queuebuy = new ArrayDeque<>();
+    Deque<Transactions> queuesell = new LinkedList<Transactions>();
+    Deque<Transactions> queuebuy = new LinkedList<Transactions>();
+
+    class ComparatorQueueSell implements Comparator<Transactions> {
+        @Override
+        public int compare(Transactions t1, Transactions t2) {
+            if (t1.getPrice() < t2.getPrice()) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    class ComparatorQueueBuy implements Comparator<Transactions> {
+        @Override
+        public int compare(Transactions t1, Transactions t2) {
+            if (t1.getPrice() < t2.getPrice()) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
 
     public boolean sell(Transactions transactions) {
-        if (checkersell()) {
-            removebuy(transactions.price);
+        if (checkerSell()) {
+
+            removeBuy(transactions);
         }
-        queuesell.addFirst(transactions);
+
+        if (checkerSell() && transactions.price < queuesell.getFirst().price) {
+
+            queuesell.addFirst(transactions);
+        }
+        queuesell.addLast(transactions);
+        Collections.sort((LinkedList) queuesell, new ComparatorQueueSell());
 
         return true;
     }
 
 
     public boolean buy(Transactions transactions) {
-        if (checkerbuy()){
-            removesell(transactions.price);
-        }       queuebuy.addFirst(transactions);
+        if (checkerBuy()) {
+
+            removeSell(transactions.price);
+        }
+
+        if (checkerBuy() && transactions.price > queuebuy.getFirst().price) {
+            queuebuy.addFirst(transactions);
+        }
+        queuebuy.addLast(transactions);
+        Collections.sort((LinkedList) queuebuy, new ComparatorQueueBuy());
 
         return true;
     }
 
-    public boolean checkersell() {
+
+    public boolean checkerSell() {
         boolean a = true;
-        if (queuebuy.isEmpty()||queuesell.isEmpty()) {
-             a = false;
+        if (queuebuy.isEmpty() || queuesell.isEmpty()) {
+            a = false;
         }
         return a;
     }
 
-    public void removebuy(Integer price) {
 
-        if (queuebuy.getFirst().price >= price) {
-            queuesell.removeFirst();
-            queuebuy.removeFirst();
+    public boolean checkerBuy() {
+        boolean a = true;
+        if (queuesell.isEmpty() || queuebuy.isEmpty()) {
+            a = false;
         }
-
+        return a;
     }
 
-    public void removesell(Integer price) {
-
+    public void removeSell(Integer price) {
         if (queuesell.getFirst().price <= price) {
             queuesell.removeFirst();
             queuebuy.removeFirst();
@@ -53,13 +87,14 @@ public class Operations {
     }
 
 
-    public boolean checkerbuy() {
-        boolean a = true;
-        if (queuesell.isEmpty()||queuebuy.isEmpty()) {
-             a = false;
-        }
-        return a;
+    public void removeBuy(Transactions transactions) {
+        if (queuebuy.getFirst().price >= transactions.price) {
+            queuesell.removeFirst();
+            queuebuy.removeFirst();
+          }
+
     }
+
 
     @Override
     public String toString() {
--- src/Start.java
+++ src/Start.java
@@ -8,19 +8,26 @@ public class Start {
         Investor Anton = new Investor("Anton", 125000);
         Investor Danila = new Investor("Danila", 225000);
 
-        System.out.println("Выберите имя");
+       // System.out.println("Выберите имя");
 
         Operations operations = new Operations();
 
 
-        Transactions transactions = new Transactions(10,"sell",2);
-        Transactions transactions2 = new Transactions(10,"buy",2);
-        operations.sell(transactions);
-        operations.buy(transactions2);
+        Transactions transactions = new Transactions(10,"buy",2);
+        Transactions transactions2 = new Transactions(12,"buy",2);
+        Transactions transactions3 = new Transactions(11,"buy",2);
         operations.buy(transactions);
+        operations.buy(transactions2);
+        operations.buy(transactions3);
+        operations.sell(transactions);
+        operations.sell(transactions2);
+        operations.sell(transactions3);
+       // operations.buy(transactions2);
+        //operations.buy(transactions);
 
        // operations.checkerbuy(transactions2.price,transactions2.type);
         System.out.println(operations.queuebuy);
         System.out.println(operations.queuesell);
+
     }
 }
